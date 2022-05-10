756afe46a022fd143e459ba7881ab6bf3c054994
thepassle <pascalschilp@gmail.com>
2022-05-10 17:38:24 +0200

    chore: rename

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/edge-functions';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: netlify()
+});
